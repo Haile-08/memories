7a7c9e63d3c13d5e59b66b28a50fe0cc07e992c4
Haile-08 <haile4cmd@gmail.com>
2022-08-05 16:40:02 +0300

    created the redux store

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,12 +1,12 @@
 import React from "react";
-import post from "./post/post";
+import Post from "./post/post";
 import useStyles from "./styles";
 function Posts() {
   const classes = useStyles();
   return (
     <>
       <h1>posts</h1>
-      <post />
+      <Post />
     </>
   );
 }
